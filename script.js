32e01900fa5c8d25f5eb8805d4900385ffc2584c
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-06-12 23:11:40 +0530

    Finished

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // TODO:Getting DOM elements here
 
-const allActivityTypes=document.querySelectorAll(".item");
+const allActivityTypes = document.querySelectorAll(".item");
+const activityContent = document.querySelector(".activityContent");
+const participantsNumber = document.querySelector(".participants");
 
 // TODO: Logic here 
 
-let activityType="education";
+let activityType = "education";
 let url = "http://www.boredapi.com/api/activity";
-function constructUrl(activity){
+function constructUrl(activity) {
     return `${url}?type=${activity}`
 }
 allActivityTypes.forEach(element => {
-    element.addEventListener('click',()=>{
-        activityType=(element.innerText).toLowerCase();
+    element.addEventListener('click', () => {
+        activityType = (element.innerText).toLowerCase();
         console.log(activityType);
         fetch(constructUrl(activityType))
-        .then(response=>response.json())
-        .then(data=>console.log(data))
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                activityContent.innerHTML=`Your Task : ${data.activity}`;
+                participantsNumber.innerHTML =`Needed Participants : ${data.participants}`;
+            })
     })
 });
 
